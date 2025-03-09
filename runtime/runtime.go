f86dca42c95856f422f976d1d11d3a74d6055d3f
JT Archie <jtarchie@gmail.com>
2025-03-08 18:14:47 -0700

    refactor example test, then fix data race with promises

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -2,45 +2,53 @@ package runtime
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/dop251/goja"
 	"github.com/google/uuid"
-	"golang.org/x/sync/errgroup"
 )
 
 type Runtime struct {
-	promises *errgroup.Group
-	sandbox  *PipelineRunner
 	jsVM     *goja.Runtime
+	promises *sync.WaitGroup
+	sandbox  *PipelineRunner
+	tasks    chan func() error
 }
 
 func NewRuntime(
 	jsVM *goja.Runtime,
 	sandbox *PipelineRunner,
 ) *Runtime {
-	promises := &errgroup.Group{}
-	// promises.SetLimit(1)
+	promises := &sync.WaitGroup{}
+	tasks := make(chan func() error, 1)
 
 	return &Runtime{
+		jsVM:     jsVM,
 		promises: promises,
 		sandbox:  sandbox,
-		jsVM:     jsVM,
+		tasks:    tasks,
 	}
 }
 
 func (r *Runtime) Run(input RunInput) *goja.Promise {
 	promise, resolve, _ := r.jsVM.NewPromise()
 
-	r.promises.Go(func() error {
+	r.promises.Add(1)
+
+	go func() {
 		result := r.sandbox.Run(input)
 
-		err := resolve(result)
-		if err != nil {
-			return fmt.Errorf("could not resolve: %w", err)
-		}
+		r.tasks <- func() error {
+			defer r.promises.Done()
 
-		return nil
-	})
+			err := resolve(result)
+			if err != nil {
+				return fmt.Errorf("could not resolve run: %w", err)
+			}
+
+			return nil
+		}
+	}()
 
 	return promise
 }
@@ -52,24 +60,37 @@ func (r *Runtime) CreateVolume(input VolumeInput) *goja.Promise {
 
 	promise, resolve, _ := r.jsVM.NewPromise()
 
-	r.promises.Go(func() error {
+	r.promises.Add(1)
+
+	go func() {
 		result := r.sandbox.CreateVolume(input)
 
-		err := resolve(result)
-		if err != nil {
-			return fmt.Errorf("could not resolve: %w", err)
-		}
+		r.tasks <- func() error {
+			defer r.promises.Done()
 
-		return nil
-	})
+			err := resolve(result)
+			if err != nil {
+				return fmt.Errorf("could not resolve create volume: %w", err)
+			}
+
+			return nil
+		}
+	}()
 
 	return promise
 }
 
 func (r *Runtime) Wait() error {
-	err := r.promises.Wait()
-	if err != nil {
-		return fmt.Errorf("could not wait: %w", err)
+	go func() {
+		r.promises.Wait()
+		close(r.tasks)
+	}()
+
+	for task := range r.tasks {
+		err := task()
+		if err != nil {
+			return fmt.Errorf("could not wait: %w", err)
+		}
 	}
 
 	return nil
