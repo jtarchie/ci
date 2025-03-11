21d0af13329400ced8eb8dbc19475b77c4c4c33e
JT Archie <jtarchie@gmail.com>
2025-03-11 13:03:31 -0600

    add test for assert, fix assertion usage

---
+++ runtime/assert_test.go
@@ -0,0 +1,45 @@
+package runtime_test
+
+import (
+	"log/slog"
+	"strings"
+	"testing"
+
+	"github.com/dop251/goja"
+	"github.com/jtarchie/ci/runtime"
+	. "github.com/onsi/gomega"
+)
+
+func TestAssert(t *testing.T) {
+	t.Parallel()
+
+	assert := NewGomegaWithT(t)
+
+	jsVM := goja.New()
+	err := jsVM.Set("assert", runtime.NewAssert(jsVM, slog.Default()))
+	assert.Expect(err).ToNot(HaveOccurred())
+	_, err = jsVM.RunString(strings.TrimSpace(`
+		assert.Equal(1, 1);
+		assert.NotEqual(1, 2);
+		assert.ContainsString("foobar", "foo");
+		assert.Truthy(true);
+		assert.ContainsElement([1, 2, 3], 2);
+	`))
+	assert.Expect(err).ToNot(HaveOccurred())
+
+	failures := []string{
+		"assert.Equal(1, 2);",
+		"assert.NotEqual(1, 1);",
+		"assert.ContainsString('foobar', 'baz');",
+		"assert.Truthy(false);",
+		"assert.ContainsElement([1, 2, 3], 4);",
+	}
+
+	for _, failure := range failures {
+		_, err = jsVM.RunString(strings.TrimSpace(failure))
+		assert.Expect(err).To(HaveOccurred(), "expected error for "+failure)
+
+		errString := err.Error()
+		assert.Expect(errString).To(ContainSubstring("assertion failed"))
+	}
+}
