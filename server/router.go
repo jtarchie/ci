3e5f801c2502f582f2edf71cb3956375850e7c15
JT Archie <jtarchie@gmail.com>
2025-04-13 19:20:29 -0600

    move server code in server package

---
+++ server/router.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	slogecho "github.com/samber/slog-echo"
+)
+
+func NewRouter(logger *slog.Logger) (*echo.Echo, error) {
+	router := echo.New()
+	router.Use(slogecho.New(logger))
+	router.Use(middleware.Recover())
+
+	renderer, err := newTemplates()
+	if err != nil {
+		return nil, fmt.Errorf("could not create templates: %w", err)
+	}
+
+	router.Renderer = renderer
+
+	router.GET("/health", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "OK")
+	})
+
+	return router, nil
+}
---
+++ server/templates.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"embed"
+	"fmt"
+	"io"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	sprig "github.com/go-task/slim-sprig/v3"
+	"github.com/labstack/echo/v4"
+)
+
+//go:embed templates/*
+var templatesFS embed.FS
+
+type TemplateRender struct {
+	templates *template.Template
+}
+
+func (t *TemplateRender) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	err := t.templates.ExecuteTemplate(w, name, data)
+	if err != nil {
+		return fmt.Errorf("could not execute template: %w", err)
+	}
+
+	return nil
+}
+
+func newTemplates() (*TemplateRender, error) {
+	templates, err := template.New("templates").
+		Funcs(sprig.FuncMap()).
+		Funcs(template.FuncMap{
+			"formatPath": func(path string) string {
+				path = strings.ReplaceAll(path, " ", "")
+				path = filepath.Clean(path)
+				if path[0] != '/' {
+					path = "/" + path
+				}
+
+				return strings.ReplaceAll(path, "/", " / ")
+			},
+		}).
+		ParseFS(templatesFS, "templates/*")
+	if err != nil {
+		return nil, fmt.Errorf("could not parse templates: %w", err)
+	}
+
+	renderer := &TemplateRender{
+		templates: templates,
+	}
+
+	return renderer, nil
+}
