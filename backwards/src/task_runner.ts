d6177f5a7dc71cab40627d0d08938db7acf0debc
JT Archie <jtarchie@gmail.com>
2025-10-26 09:02:43 -0600

    backfill some tests

--- backwards/src/task_runner.ts
+++ backwards/src/task_runner.ts
@@ -33,7 +33,7 @@ export class TaskRunner {
           args: step.config.run.args || [],
           user: step.config.run.user,
         },
-        container_limits: step.container_limits ?? step.config.container_limits,
+        container_limits: step.config.container_limits,
         env: step.config.env,
         image: step.config?.image_resource.source.repository!,
         name: step.task,
