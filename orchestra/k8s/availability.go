adde06f9b08b92b1157262cf9d30d8bdfee7e9bd
JT Archie <jtarchie@gmail.com>
2026-01-16 12:04:44 -0700

    fix some flakey tests

---
+++ orchestra/k8s/availability.go
@@ -0,0 +1,32 @@
+package k8s
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// IsAvailable checks if a Kubernetes cluster is available and reachable.
+// This is a lightweight check that only verifies API server connectivity.
+func IsAvailable() bool {
+	// Try to create a client
+	client, err := NewK8s("availability-check", slog.Default(), map[string]string{})
+	if err != nil {
+		return false
+	}
+	defer func() { _ = client.Close() }()
+
+	// Simple API call to verify connectivity - just check if we can list namespaces
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	k8sClient, ok := client.(*K8s)
+	if !ok {
+		return false
+	}
+
+	_, err = k8sClient.clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{Limit: 1})
+	return err == nil
+}
